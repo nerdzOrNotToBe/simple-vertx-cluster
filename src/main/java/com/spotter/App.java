30c45f911e0ce333ec6de069cc7d4d19395d13b6
dimitri <lebel.dimitri@gmail.com>
2016-11-12 12:58:13 +0100

    change way to start cluster

--- src/main/java/com/spotter/App.java
+++ src/main/java/com/spotter/App.java
@@ -1,10 +1,6 @@
 package com.spotter;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
-import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
 /**
  * Created by dimitri on 03/11/2016.
@@ -12,17 +8,7 @@ import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 public class App extends AbstractVerticle {
 
 	public void start() {
-		ClusterManager mgr = new HazelcastClusterManager();
 
-		VertxOptions options = new VertxOptions().setClusterManager(mgr);
-
-		Vertx.clusteredVertx(options, res -> {
-			if (res.succeeded()) {
-				super.vertx = res.result();
-			} else {
-				// failed!
-			}
-		});
 	}
 
 
